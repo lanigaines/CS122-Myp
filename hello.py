aab6fa44a9fb5c64354738479a31d9c48f9589dc
lanigaines <lanigaines@live.com>
2024-09-11 20:19:38 -0700

    homework

--- hello.py
+++ hello.py
@@ -4,4 +4,10 @@ print (" ' ")
 print (' " ') 
 print(' " ') # This will give an error
 print(' " ') # This will give an error
-
+num = input ('100') #
+print (num)
+sum = num + 100
+print (sum)
+ 
+ #The output of the above code is: 
+ #Hello World
